772d09dde9fda2caf2b4d3531ec925cb1dc8580c
Justin Rowles <justin.rowles@gmail.com>
2025-01-18 14:45:39 +0000

    Add Tom Lehrer quote

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,14 @@ const Footer: React.FC = () => {
   return (
     <footer>
       <p> (c) Justin Rowles - Not Dead Yet</p>
+      <br/>
+      <br/>
+      <br/>
+      <em>
+      It is a sobering thought that when Wolfgang Amadeus Mozart was my age, he had been dead for two years.
+      <br/>
+      ~Tom Lehrer
+      </em>
     </footer>
   );
 };
